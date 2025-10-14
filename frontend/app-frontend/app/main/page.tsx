c4e0211049773ba72203c142786b2678e012a6c9
JBroook <JeremiahBoey@gmail.com>
2025-10-14 15:50:37 +0800

    Added folder icon image

--- frontend/app-frontend/app/main/page.tsx
+++ frontend/app-frontend/app/main/page.tsx
@@ -1,22 +1,41 @@
 'use client'
-import { Box } from "@chakra-ui/react";
 import SimpleSidebar from "@/components/ui/sidebars/Sidebar";
 import { useColorModeValue } from "@/components/ui/color-mode";
-// import { usePathname } from 'next/navigation';
+import { 
+  Box, 
+  Heading,
+  Image,
+  Input, 
+  Stack,
+  Button,
+  HStack
+} from "@chakra-ui/react"
 
-export default function Home() {
-  // const pathname = usePathname();
-  // const noSidebarPages = ['/login'];
-  // const renderSidebar = !noSidebarPages.includes(pathname);
+export default function Main() {
 
   return (
     <>
-    {/* <Box 
+    <Box 
     w={"100vw"}
     h={"100vh"}
-    bgGradient="to-b" gradientFrom={useColorModeValue('blue.300', 'blue.800')} gradientTo={useColorModeValue('blue.100', 'blue.950')}>
-      {renderSidebar && <SimpleSidebar  />}
-    </Box> */}
+    bgGradient="to-b" gradientFrom={useColorModeValue('blue.300', 'blue.800')} 
+    gradientTo={useColorModeValue('blue.100', 'blue.950')}>
+      {/* Header box for title, search bar and others */}
+      <HStack 
+      w="100vw"
+      ml="80px"
+      h="12vh">
+        <HStack>
+          <Image width={5} src="/folder_icon.png" alt="folder icon" />
+          <Heading
+          fontFamily="var(--font-roboto-condensed)"
+          fontStyle="italic"
+          color="black"
+          size={"2xl"}
+          >Your Files</Heading>
+        </HStack>
+      </HStack>
+    </Box>
   </>
   );
 }
