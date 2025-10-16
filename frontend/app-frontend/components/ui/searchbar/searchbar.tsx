53353b834036eb29abc8896b9eda94a36bce4e9b
JBroook <JeremiahBoey@gmail.com>
2025-10-16 13:53:26 +0800

    Search bar and filter bar options ui completed

---
+++ frontend/app-frontend/components/ui/searchbar/searchbar.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from 'react'
+import { Input
+} from "@chakra-ui/react"
+
+interface SearchbarProps {
+  placeholder: string;
+}
+
+export default function Searchbar(props: SearchbarProps){
+    return (<Input 
+        type="text"
+        placeholder={props.placeholder}
+        variant="flushed" 
+        pl={2}
+        color="white"
+        _placeholder={{ color: "white"}}
+        data-state="open"
+        _open={{
+            animationName: "fade-in, scale-in",
+            animationDuration: "300ms",
+    }}/>);
+}
