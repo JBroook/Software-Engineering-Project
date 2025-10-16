392eea4e376f3777174715a65baa3e9a01a0385b
JBroook <JeremiahBoey@gmail.com>
2025-10-16 13:53:54 +0800

    Search bar and filter bar options ui completed

--- frontend/app-frontend/app/main/page.tsx
+++ frontend/app-frontend/app/main/page.tsx
@@ -1,18 +1,30 @@
 'use client'
 import GalleryItem from "@/components/ui/item/galleryItem";
 import GalleryFolder from "@/components/ui/item/galleryFolder";
+import Searchbar from "@/components/ui/searchbar/searchbar";
 import { useColorModeValue } from "@/components/ui/color-mode";
 import { 
   Box, Heading, Image, Grid,
   GridItem, HStack, Flex, SimpleGrid,
-  Separator,
-  IconButton
+  Separator, IconButton, Input, Popover,
+  Text
 } from "@chakra-ui/react"
 import { FaFolder } from "react-icons/fa";
 import { FaFile } from "react-icons/fa";
 import { IoIosArrowBack } from "react-icons/io";
+import { IoSearchCircleOutline } from "react-icons/io5";
+import { IoFilter } from "react-icons/io5";
+import { RiGalleryView2 } from "react-icons/ri";
+import { IoIosList } from "react-icons/io";
+import { useState } from "react";
 
 export default function Main() {
+  const [searchbar, setSearchbar] = useState(true);
+  const [viewType, setViewType] = useState("gallery");
+
+  const changeViewType = () => {
+    setViewType(viewType=="gallery"?"list" : "gallery" );
+  }
 
   return (
     <Box bg="white" minH="100vh">
@@ -37,6 +49,44 @@ export default function Main() {
           size={"3xl"}
           >Audio / Animal sounds / Mammal roars</Heading>
         </HStack>
+
+        <HStack mr={10}>
+            <IconButton borderRadius={"xl"} bg="#F6F6F6" cursor="pointer"
+            _hover={{ bg: '#e0e0e0ff' }}
+            onClick={() => setSearchbar(!searchbar)}>
+              <IoSearchCircleOutline color="#9AB3F2" size={"sm"}/>
+            </IconButton>
+            
+            {searchbar && <Searchbar placeholder="Search a file"/>}
+
+            <Popover.Root>
+              <Popover.Trigger asChild>
+                <IconButton borderRadius={"xl"} bg="#F6F6F6" cursor="pointer"
+                  _hover={{ bg: '#e0e0e0ff' }}>
+                  <IoFilter color="#9AB3F2"/>
+                </IconButton>
+              </Popover.Trigger>
+              <Popover.Positioner>
+                <Popover.Content>
+                  <Popover.CloseTrigger />
+                  <Popover.Arrow>
+                    <Popover.ArrowTip />
+                  </Popover.Arrow>
+                  <Popover.Body p={3}>
+                    <Popover.Title color="black" fontWeight="medium">Filter options</Popover.Title>
+                    
+                  </Popover.Body>
+                </Popover.Content>
+              </Popover.Positioner>
+            </Popover.Root>
+
+            <IconButton borderRadius={"xl"} bg="#F6F6F6" cursor="pointer"
+            _hover={{ bg: '#e0e0e0ff' }}
+            onClick={changeViewType}>
+              {viewType=="gallery"?<RiGalleryView2 color="#9AB3F2"/>:<IoIosList color="#9AB3F2"/>}
+            </IconButton>
+
+          </HStack>
       </Flex>
 
       {/* Gallery view */}
@@ -72,9 +122,7 @@ export default function Main() {
           date="28 September 2025"/>
       </SimpleGrid>
 
-      <Separator size={"md"}>
-
-      </Separator>
+      <Separator size={"md"} />
 
       {/* Files */}
       <HStack
--- launch_bck.sh
+++ launch_bck.sh
@@ -4,12 +4,3 @@ pipenv shell
 
 py manage.py runserver
 
-cd ..
-
-cd frontend
-
-cd app-frontend || exit 1
-
-npm run dev
-
-echo "Website launched"
