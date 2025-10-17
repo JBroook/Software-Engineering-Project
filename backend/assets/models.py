5770519d7481f49eca2dcaac45e98755f5b83e74
JBroook <JeremiahBoey@gmail.com>
2025-10-17 16:27:16 +0800

    Created foundation for backend models

--- backend/assets/models.py
+++ backend/assets/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Folder(models.Model):
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    folder = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.foldername
+
+class File(models.Model):
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    size = models.IntegerField()
+    filetype = models.CharField(max_length=5)
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.filename
+
+class TagType(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    file = models.ForeignKey(File, on_delete=models.CASCADE)
+    type = models.ForeignKey(TagType, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.type.name
